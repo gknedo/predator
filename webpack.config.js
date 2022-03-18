1d2000c57243072eb1d127ac2e572787f1bcedae
Gustavo Canedo <gustavo.canedo@betrybe.com>
2022-03-18 18:07:20 -0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
